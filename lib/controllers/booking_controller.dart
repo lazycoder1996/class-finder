078023fc867c445fcbea66f2ebf0023e049f5ab6
sbarhin <shaubint123@gmail.com>
2023-07-25 08:02:26 +0000

    first commit

--- lib/controllers/booking_controller.dart
+++ lib/controllers/booking_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:timetable_app/controllers/user_controller.dart';
 import 'package:timetable_app/data/models/body/booking_body.dart';
 import 'package:timetable_app/data/models/responses/booking_model.dart';
 import 'package:timetable_app/data/models/responses/response_model.dart';
@@ -7,17 +8,41 @@ import '../data/repository/booking_repo.dart';
 
 class BookingController extends GetxController implements GetxService {
   final BookingRepo bookingRepo;
+
   BookingController(this.bookingRepo);
 
   bool _creating = false;
+
   bool get creating => _creating;
+
+  // Delete booking
+  bool _deleting = false;
+
+  bool get deleting => _deleting;
+
+  Future<ResponseModel> deleteBooking(int bookingId) async {
+    _deleting = true;
+    update();
+    ResponseModel responseModel;
+    Response response = await bookingRepo.deleteBooking(bookingId);
+    if (response.statusCode == 200) {
+      responseModel = ResponseModel(true, 'Booking deleted');
+      getBookings({
+        'reference': Get.find<UserController>().user?.reference,
+      });
+    } else {
+      responseModel = ResponseModel(false, 'An error occurred');
+    }
+    _deleting = false;
+    update();
+    return responseModel;
+  }
 
   // Add booking
   Future<ResponseModel> createBooking(BookingBody bookingBody) async {
     _creating = true;
     update();
     ResponseModel responseModel;
-
     Response response = await bookingRepo.addBooking(bookingBody);
     // Response response = await bookingRepo.addBooking(bookingBody);
     print(response.body);
@@ -40,7 +65,9 @@ class BookingController extends GetxController implements GetxService {
 
   // Get user bookings
   List<BookingModel>? _bookings;
+
   List<BookingModel>? get bookings => _bookings;
+
   Future<void> getBookings(Map<String, dynamic> query) async {
     Response response = await bookingRepo.getUserBookings(query);
     if (response.statusCode == 200) {
--- lib/views/screens/booking/widgets/booking_widget.dart
+++ lib/views/screens/booking/widgets/booking_widget.dart
@@ -1,76 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:timetable_app/controllers/booking_controller.dart';
 import 'package:timetable_app/data/models/responses/booking_model.dart';
 import 'package:timetable_app/helpers/extensions.dart';
+import 'package:timetable_app/utils/app_colors.dart';
+import 'package:timetable_app/utils/navigation.dart';
 
 import '../../../../helpers/font_styles.dart';
+import '../../../base/custom_snackbar.dart';
 
-class BookingWidget extends StatelessWidget {
+class BookingWidget extends StatefulWidget {
   final BookingModel booking;
+  final BookingController controller;
+
   const BookingWidget({
     super.key,
     required this.booking,
+    required this.controller,
   });
 
   @override
+  State<BookingWidget> createState() => _BookingWidgetState();
+}
+
+class _BookingWidgetState extends State<BookingWidget> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Card(
-        elevation: 2.5,
-        margin: EdgeInsets.zero,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Container(
-          height: 115,
-          width: double.maxFinite,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            // color: AppColors.secondary,
+    return Slidable(
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        extentRatio: 0.2,
+        children: [
+          SlidableAction(
+            // borderRadius: BorderRadius.circular(15),
+            padding: EdgeInsets.zero,
+            onPressed: (context) async {
+              showDialog<void>(
+                context: context,
+                barrierDismissible: true,
+
+                // false = user must tap button, true = tap outside dialog
+                builder: (BuildContext dialogContext) {
+                  return AlertDialog(
+                    title: Text('delete'),
+                    content: widget.controller.deleting
+                        ? const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : Text('Would you want to delete this card?'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('Yes'),
+                        onPressed: () async {
+                          await widget.controller
+                              .deleteBooking(widget.booking.id)
+                              .then((status) {
+                            showCustomSnackBar(status.message,
+                                isError: !status.isSuccess);
+                            pop(dialogContext);
+                          });
+                        },
+                      ),
+                      TextButton(
+                        child: Text('No'),
+                        onPressed: () {
+                          Navigator.of(dialogContext)
+                              .pop(); // Dismiss alert dialog
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            foregroundColor: AppColors.red,
+            backgroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+      endActionPane: ActionPane(
+        extentRatio: 0.2,
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: Colors.white,
+            foregroundColor: AppColors.red,
+            icon: Icons.edit,
+            label: 'Edit',
+          ),
+        ],
+      ),
+      key: Key(widget.booking.id.toString()),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Card(
+          elevation: 2.5,
+          margin: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
           ),
-          child: Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: Container(
-                  alignment: Alignment.center,
-                  width: 96,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(
-                    booking.day.substring(0, 3).toUpperCase(),
-                    style: bold(22).copyWith(color: Colors.white),
-                    textAlign: TextAlign.center,
+          child: Container(
+            height: 115,
+            width: double.maxFinite,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              // color: AppColors.secondary,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 96,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      widget.booking.day.substring(0, 3).toUpperCase(),
+                      style: bold(22).copyWith(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ),
-              ),
-              15.w,
-              Flexible(
-                flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      '${booking.course.code} ${booking.course.name}',
-                      overflow: TextOverflow.ellipsis,
-                      style: bold(18),
-                    ),
-                    Text(
-                      booking.room,
-                      style: semiBold(17),
-                    ),
-                    Text(
-                      '${booking.startTime.toTime} - ${booking.endTime.toTime}',
-                      style: regular(16),
-                    )
-                  ],
-                ),
-              )
-            ],
+                15.w,
+                Flexible(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '${widget.booking.course.code} ${widget.booking.course.name}',
+                        overflow: TextOverflow.ellipsis,
+                        style: bold(18),
+                      ),
+                      Text(
+                        widget.booking.room,
+                        style: semiBold(17),
+                      ),
+                      Text(
+                        '${widget.booking.startTime.toTime} - ${widget.booking.endTime.toTime}',
+                        style: regular(16),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
